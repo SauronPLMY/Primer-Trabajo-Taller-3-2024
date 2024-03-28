fa00dfd29a976c1ebb96b7393390aec1086401da
Nico Santi <nicolas.santibanezv@alumnos.arcos.cl>
2024-03-28 18:16:58 -0300

    Clase 1 Boceto

--- Assets/Scripts/ControlesPlayer.cs
+++ Assets/Scripts/ControlesPlayer.cs
@@ -103,7 +103,7 @@ public class ControlesPlayer : MonoBehaviour
             StartCoroutine(CheckAterrizaje());
         }
 
-        horizontal = Input.GetAxis("Horizontal") * velocidadMovimiento ;
+        horizontal =  velocidadMovimiento ;
         SonidosPaso();
     }
 
